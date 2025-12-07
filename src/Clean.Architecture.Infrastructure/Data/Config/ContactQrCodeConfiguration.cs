273b6c2a3ae6e71cb51d1e68831e5a56ee89ac9f
Ibrahim Samir Ayad Azer <ibrahim.samir.ayad@hotmail.com>
2025-12-07 14:44:26 +0300

    Generate Contact QR and sve it to Database

---
+++ src/Clean.Architecture.Infrastructure/Data/Config/ContactQrCodeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Clean.Architecture.Core.ContactQrCodeAggregate;
+
+namespace Clean.Architecture.Infrastructure.Data.Config;
+
+internal class ContactQrCodeConfiguration : IEntityTypeConfiguration<ContactQrCode>
+{
+  public void Configure(EntityTypeBuilder<ContactQrCode> builder)
+  {
+
+    builder.HasKey(e => e.Id);
+    builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+    builder.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(20);
+    builder.Property(e => e.QrImage).IsRequired();
+  }
+}
